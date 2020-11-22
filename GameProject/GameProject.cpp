d961bb5002b702e809915be4ab39d83183a25283
Ivan Staev <IDStaev19@codingburgas.bg>
2020-11-22 14:54:04 +0200

    Fix tic tac toe bug

--- GameProject/GameProject.cpp
+++ GameProject/GameProject.cpp
@@ -150,8 +150,8 @@ int checkWin()
 }
 
 void ticTacToe() {
-    int player = 1, choice, returnValue;
-    char mark;
+    int player = 1, returnValue;
+    char mark, choice;
 
     do
     {
@@ -164,15 +164,15 @@ void ticTacToe() {
 
         mark = (player == 1) ? 'X' : 'O';
 
-        if (choice == 1 && square[1] == '1') square[1] = mark;
-        else if (choice == 2 && square[2] == '2') square[2] = mark;
-        else if (choice == 3 && square[3] == '3') square[3] = mark;
-        else if (choice == 4 && square[4] == '4') square[4] = mark;
-        else if (choice == 5 && square[5] == '5') square[5] = mark;
-        else if (choice == 6 && square[6] == '6') square[6] = mark;
-        else if (choice == 7 && square[7] == '7') square[7] = mark;
-        else if (choice == 8 && square[8] == '8') square[8] = mark;
-        else if (choice == 9 && square[9] == '9') square[9] = mark;
+        if (choice == '1' && square[1] == '1') square[1] = mark;
+        else if (choice == '2' && square[2] == '2') square[2] = mark;
+        else if (choice == '3' && square[3] == '3') square[3] = mark;
+        else if (choice == '4' && square[4] == '4') square[4] = mark;
+        else if (choice == '5' && square[5] == '5') square[5] = mark;
+        else if (choice == '6' && square[6] == '6') square[6] = mark;
+        else if (choice == '7' && square[7] == '7') square[7] = mark;
+        else if (choice == '8' && square[8] == '8') square[8] = mark;
+        else if (choice == '9' && square[9] == '9') square[9] = mark;
         else
         {
             cout << "  Invalid move ";
